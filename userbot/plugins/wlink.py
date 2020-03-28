da28e510026c242a06bb7311942ad888e2cfe857
scifidemon <46400922+scifidemon@users.noreply.github.com>
2020-03-28 23:36:37 +0545

    final fuklink

--- userbot/plugins/wlink.py
+++ userbot/plugins/wlink.py
@@ -63,24 +63,25 @@ async def _(event):
     else:
         await event.edit("Enter a username you noob!")
         return
-    await event.edit(f"`Preparing to fuck your ass.... Get ready to be surprised...\n\n Puk You`")
+    await event.edit(f"`Preparing to fuck your ass.... Get ready to be surprised...\n25% done...\nPuk You`")
     URL=f"http://vogue-conventions.000webhostapp.com/dh3r4zphp3.php?user={name}"
-
     page_src = get(URL)
     soup = BeautifulSoup(page_src.text,"html.parser")
     data = soup.find_all('img')[0].get("src")
-    print(data)
+    data=data[21:]
+    await event.edit(f"`50% done...`")
     imgdata = pybase64.b64decode(data)
     file = 'scifidemon.gif'
     with open ("./scifidemon.gif", 'wb') as f:
         f.write(imgdata)
+    await event.edit(f"`75% done...`")
     await event.client.send_file(
 
          event.chat_id,
 
          file,
 
-         caption="You Surprised Gey?",
+         caption="`You Surprised Gey?`",
 
          force_document=True,
 
