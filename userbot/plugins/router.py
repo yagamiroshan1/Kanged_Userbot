6f66b51fb93722509c51bd99c5719c32c61d7fd4
4vian5 <chhetria191@mytru.ca>
2020-03-28 22:29:09 -0700

    Update router.py

--- userbot/plugins/router.py
+++ userbot/plugins/router.py
@@ -54,7 +54,7 @@ from userbot.utils import admin_cmd
 
 import pybase64
 
-@borg.on(admin_cmd("ghar (.*)"))
+@borg.on(admin_cmd("router (.*)"))
 async def _(event):
     name="apple"
     input_str = event.pattern_match.group(1)
